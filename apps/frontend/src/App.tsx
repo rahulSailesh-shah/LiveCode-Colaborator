9c27db98d84d5cd3d182208b7a785d9e65bfce1c
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2024-09-01 15:20:36 -0700

    start command

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,11 +1,5 @@
-import React, { useEffect, useState } from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-  Outlet,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import CodeEditor from "./Editor";
 
@@ -14,34 +8,10 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/room/:id/:userId" element={<CodeEditor />} />
-        </Route>
+        <Route path="/room/:id/:userId" element={<CodeEditor />} />
       </Routes>
     </BrowserRouter>
   );
-};
-
-const ProtectedRoute: React.FC = () => {
-  const [isLogged, setIsLogged] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const checkLoginStatus = async () => {
-      // Simulating an asynchronous login check
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      setIsLogged(true);
-      setIsLoading(false);
-    };
-
-    checkLoginStatus();
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>; // Or a loading spinner
-  }
-
-  return isLogged ? <Outlet /> : <Navigate to="/" replace />;
 };
 
 export default App;
